8b36a6d3ae6d2917782f1147aae7ce6d3facec1c
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-04-14 11:44:24 -0300

    adicionar transação

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDto {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,8 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDto): Transaction {
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
